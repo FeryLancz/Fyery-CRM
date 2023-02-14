4ccb789653acd836705386e18a2021875adeb845
FeryLancz <fery.lancz@me.com>
2023-02-14 21:16:43 +0100

    Firebase code cleanup

--- Fyery/Core/Model/FyeryModelFirebaseDatabase.swift
+++ Fyery/Core/Model/FyeryModelFirebaseDatabase.swift
@@ -25,43 +25,15 @@ extension FyeryModel {
     }
     
     func postThought() {
-        guard let databasePath = databasePath else {
-            return
-        }
-        if user == nil {
-            return
-        }
-        //let thought = ThoughtModel(text: newThoughtText)
+        guard let databasePath = databasePath else { return }
+        if user == nil { return }
         do {
             let data = try JSONEncoder().encode(user)
             let json = try JSONSerialization.jsonObject(with: data)
             databasePath.child(String(user.id))
                 .setValue(json)
         } catch {
-            print("an error occurred", error)
+            print("Error:", error)
         }
     }
-    
-    //    func listenForThoughts() {
-    //        guard let databasePath = databasePath else {
-    //            return
-    //        }
-    //        databasePath
-    //            .observe(.childAdded) { [weak self] snapshot in
-    //                guard
-    //                    let self = self,
-    //                    var json = snapshot.value as? [String: Any]
-    //                else {
-    //                    return
-    //                }
-    //                json["id"] = snapshot.key
-    //                do {
-    //                    let thoughtData = try JSONSerialization.data(withJSONObject: json)
-    //                    let thought = try self.decoder.decode(ThoughtModel.self, from: thoughtData)
-    //                    self.thoughts.append(thought)
-    //                } catch {
-    //                    print("an error occurred", error)
-    //                }
-    //            }
-    //    }
 }
